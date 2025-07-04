8ef0527e411ee1f5a0c093f15267da7bdc44b593
Eve-code93 <evelynejose1993@gmail.com>
2025-07-05 00:21:30 +0300

    fix aggregation to add rank

--- database-adv-script/aggregations_and_window_functions.sql
+++ database-adv-script/aggregations_and_window_functions.sql
@@ -8,13 +8,15 @@ LEFT JOIN bookings b ON u.id = b.user_id
 GROUP BY u.id, u.name
 ORDER BY total_bookings DESC;
 
-ased on number of bookings using ROW_NUMBER()
+
+-- 2. Window Function: Rank properties based on number of bookings using RANK()
 SELECT 
     p.id AS property_id,
     p.name AS property_name,
     COUNT(b.id) AS total_bookings,
-    ROW_NUMBER() OVER (ORDER BY COUNT(b.id) DESC) AS booking_rank
+    RANK() OVER (ORDER BY COUNT(b.id) DESC) AS booking_rank
 FROM properties p
 LEFT JOIN bookings b ON p.id = b.property_id
 GROUP BY p.id, p.name
 ORDER BY booking_rank;
+
